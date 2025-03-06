90b6a2ac317cbf00064bbe62f3dd6f38bf6e5cd9
ANKITKUMAR1809 <skwedsak143@gmail.com>
2025-03-07 00:28:51 +0530

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ const App = () => {
   const handleInput = (e) => {
     setInput(e.target.value);
   }
-  const handleOnclick = () => {
+  const handleOnclick = (e) => {
+    e.preventDefault();
     const upperInput = input.toUpperCase().trim();
     setTodos([...todos, upperInput])
     setInput('');
@@ -17,7 +18,7 @@ const App = () => {
     setTodos(newTodos);
   }
   const handleDeleteAll = () => {
-    if(todos.length === 0){
+    if (todos.length === 0) {
       alert('There is no todo to delete');
       return;
     }
@@ -27,19 +28,21 @@ const App = () => {
     <div className='h-screen w-screen flex flex-col justify-center items-center bg-zinc-50'>
       <div className='mt-8 md:w-2/5 w-[90%] shadow-2xl bg-white  px-8 py-8 rounded-2xl overflow-hidden'>
         <h1 className='text-4xl font-semibold my-8 text-center'>TO-DO APP</h1>
-        <div className='my-4 w-full'>
-          <input
-            className='border border-gray-400 p-2 rounded-2xl w-full text-2xl'
-            type="text"
-            value={input}
-            placeholder='Enter your todo here'
-            onChange={handleInput}
-            required
-          />
-        </div>
-        <div>
-          <button className='hover:scale-102 transition-all active:scale-97 w-full bg-zinc-400 py-2 rounded-2xl text-white text-2xl' onClick={handleOnclick}>Add Todo</button>
-        </div>
+        <form onSubmit={handleOnclick}>
+          <div className='my-4 w-full'>
+            <input
+              className='border border-gray-400 p-2 rounded-2xl w-full text-2xl'
+              type="text"
+              value={input}
+              placeholder='Enter your todo here'
+              onChange={handleInput}
+              required
+            />
+          </div>
+          <div>
+            <input type='Submit' className='hover:scale-102 transition-all active:scale-97 w-full bg-zinc-400 py-2 rounded-2xl text-white text-2xl' value={'Add Todo'} />
+          </div>
+        </form>
         <div className='mt-12'>
           {todos[0] ? todos.map((todo, i) => <div className='flex justify-between font-semibold text-lg bg-zinc-700 my-4 text-white p-2 rounded-md'><p>{todo}</p> <Delete onClick={() => deleteTodo(i)} /> </div>) : <p className='text-xl font-semibold'>There is no todo..</p>}
         </div>
